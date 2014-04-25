5ef417172686c35bda0c696243fcbe85560e296e
homich77 <homich77@gmail.com>
2014-04-25 14:51:23 +0300

    -requirements

--- mysite/apps/polls/models.py
+++ mysite/apps/polls/models.py
@@ -5,12 +5,13 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __unicode__(self):              # __str__ on Python 2
         return self.question_text
         
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <  now
+        return now - datetime.timedelta(days=1) <= self.pub_date < now
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
@@ -20,5 +21,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __unicode__(self):              # __str__ on Python 2
         return self.choice_text
