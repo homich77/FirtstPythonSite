9c2c73e64674109b8eca23d6283828e0d0cb5611
homich77 <homich77@gmail.com>
2014-04-30 11:10:50 +0300

    Create Managers

---
+++ mysite/apps/cookies/managers.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.db.models import Avg
+
+#from .models import Cookie, Review
+
+
+class CookieManager(models.Manager):
+    rating_from = 4
+    rating_top = 10
+
+    def get_query_set(self):
+        cookie = super(CookieManager, self).get_query_set()
+        return cookie.annotate(avg_mark=Avg('review__mark'))
+
+    def best(self, user_obj):
+        cookie = super(CookieManager, self).get_query_set()
+        return cookie.filter(review__user_id=user_obj,
+                             review__mark__gte=self.rating_from)\
+                     .order_by("-review__mark")[:self.rating_top]
+
+
+class ReviewManager(models.Manager):
+
+    def latest_review(self, user_obj):
+        return super(ReviewManager, self).get_query_set()\
+                                         .filter(user_id=user_obj)\
+                                         .order_by("-date")[:10]
